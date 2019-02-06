81a138f141bb18bf837a54a14963ea08f8c49ee5
Cyubizz <Cyubizz@gmail.com>
2019-02-06 16:28:54 +0300

    Create index.php

---
+++ php7/index.php
@@ -0,0 +1,15 @@
+<?php
+
+function test($year){
+    if($year <=0 || $year > 9999){
+        return 'Введено неверное значение';
+    }
+
+    if(fmod($year, 4) == 0 && fmod($year, 100) != 0 || fmod($year, 400) == 0){
+        return 'Високосный год';
+    } else {
+        return 'Невисокосный год';
+    }
+}
+
+echo test(1400);
